3810dd2bda172429b85a51eab4c75e1072b50b10
dardanx <dardanxhymshiti@gmail.com>
2020-05-21 16:26:34 +0200

    add run file

---
+++ module_seed/run.py
@@ -0,0 +1,30 @@
+from module_seed.jobs import \
+    job_one_seed,\
+    job_two_seed
+from module_seed.utils.logger_utils import get_logger
+from module_seed.utils.spark_utils import create_spark_session
+
+
+jobs = {
+    'job_one_seed': job_one_seed.process,
+    'job_two_seed': job_two_seed.process
+}
+
+
+def run(parameters):
+    logger = get_logger()
+    spark = create_spark_session()
+
+    for parameter, value in parameters.items():
+        logger.info('Param {param}: {value}'.format(param=parameter, value=value))
+
+    job_name = parameters['job_name']
+
+    process_function = jobs[job_name]
+    process_function(
+        spark=spark,
+        input_bucket=parameters['input_bucket'],
+        output_bucket=parameters['output_bucket'],
+        output_bucket_prefix=parameters['output_bucket_prefix'],
+        save_mode='append'
+    )
